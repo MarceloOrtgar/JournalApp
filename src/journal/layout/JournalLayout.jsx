ce79e232b0bd2eb24bcdd99611752bd7f6277b64
Marcelo <marceloortega18@gmail.com>
2022-12-11 23:43:54 -0400

    initialize module 19

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,24 +1,27 @@
-import { Toolbar } from "@mui/material";
-import { Box } from "@mui/system";
-import { NavBar, SideBar } from "../components";
-
-const drawerWidth=240;
+import { Toolbar } from '@mui/material';
+import { Box } from '@mui/system'
+import { NavBar, SideBar } from '../components';
 
 
-export const JournalLayout = ({children}) => {
+const drawerWidth = 280;
+
+export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex' }}>
-      {/* {Navbar drawerWidth} */}
-      <NavBar drawerWidth={drawerWidth}/>
-      {/* SideBar drawerWidth */}
 
-      <SideBar drawerWidth={drawerWidth}/>
+        <NavBar drawerWidth={ drawerWidth } />
 
+        <SideBar drawerWidth={ drawerWidth } />
 
-      <Box componet='main' sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar/>
-        {children}
-      </Box>
+        <Box 
+            component='main'
+            sx={{ flexGrow: 1, p: 3 }}
+        >
+            <Toolbar />
+
+            { children }
+            
+        </Box>
     </Box>
-  );
-};
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,33 @@
-import { Typography } from "@mui/material";
-import { JournalLayout } from "../layout/JournalLayout";
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
-  <JournalLayout>
-      <Typography>
-       Esto es una prueba
+    <JournalLayout>
+      
+      {/* <Typography>Sint id officia amet velit do aliqua aliqua est ea velit minim voluptate duis laboris. Esse esse consectetur ullamco excepteur ullamco amet. Mollit est nostrud nisi irure magna dolor eiusmod aliquip aliqua nostrud incididunt enim. Velit ipsum laborum Lorem anim laboris aute ullamco ipsum do adipisicing irure.</Typography> */}
 
-      </Typography>
+      {/* <NothingSelectedView /> */}
+      <NoteView />
 
-      {/* NothingSelected */}
-      {/* NoteView */}
-</JournalLayout>
-  );
-};
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
+    </JournalLayout>
+  )
+}
